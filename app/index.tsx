0327daea6247ae62dd95829bb0b6373cf2396bec
unknown <100k1@naver.com>
2026-02-11 22:55:44 +0900

    레이아웃 설정

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.flexContainer}>
+      <Text style={styles.container_title}>Expense Tracker</Text>
+      <Text style={styles.container_subtitle}>Welcome to your budget manager</Text>
     </View>
   );
 }
+
+
+export const styles = StyleSheet.create({
+  flexContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f8f9fa",
+  },
+  container_title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#1063b7ff",
+    marginBottom: 8,
+  },
+  container_subtitle: {
+    fontSize: 16,
+    color: "#6c757d",
+  },
+});
